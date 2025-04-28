96d889261e3610cfa5f1e9e3607f17afb078714a
BIPIN SAHANI <bipinsahani786@gmail.com>
2025-04-28 12:01:11 +0530

    cors solve

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,15 @@ connectDB();
 
 const app = express();
 app.use(cors());
+
+// Enable CORS for your frontend domain
+const corsOptions = {
+    origin: 'https://info-ecom-1.onrender.com', // Your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  
+  app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
