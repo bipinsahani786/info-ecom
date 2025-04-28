a0ad0c176f3df3f309a6cca55f1da956edc08e1b
Bipin Sahani <107175682+bipinsahani786@users.noreply.github.com>
2025-04-28 11:54:34 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://info-ecom.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
